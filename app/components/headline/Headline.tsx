ca04e38e2fb9f9c5f985fe5ac96632d23dc8937a
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-05-18 08:07:50 -0300

    update: change word on headline

--- app/components/headline/Headline.tsx
+++ app/components/headline/Headline.tsx
@@ -10,7 +10,7 @@ export default function Headline() {
   return (
     <Container>
       <Div>
-        <h1>Alugue nossas placas de led e reduza seus custos operacionais de forma significativa </h1>
+        <h1>Contrate nossas placas de led e reduza seus custos operacionais de forma significativa </h1>
         <p>
           Não se preocupe com os custos iniciais, aluguel de placas de LED Ledok
           tem incluso suporte completo e manutenção.
