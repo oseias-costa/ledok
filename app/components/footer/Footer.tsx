c8e51296e40e2436eaa5555ce956f95c6ddede8c
Oséias Costa <oseiasc2@gmail.com>
2023-08-21 11:47:38 +0000

    feat(Slide): upload new photo on slide component

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import React from 'react'
-import styled from 'styled-components'
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
 import Logo from "../../../public/logo-footer.png";
 import Facebook from "../../../public/face.png";
 import Instagram from "../../../public/insta.png";
@@ -8,65 +8,84 @@ import Instagram from "../../../public/insta.png";
 export default function Footer() {
   return (
     <Container>
-        <Img src={Logo} alt='Logo LEDOK - Comunição em Leds' />
-        <Number>
-            <p>ledokadm@gmail.com</p>
-            <p>(51)9 96301314</p>
-        </Number>
-        <Midias>
-            <p>Nos acompanhe :</p>
-            <div>
-                <Image src={Facebook} alt='Facebook LEDOK' width={34} height={34} />
-                <Image src={Instagram} alt='Instagram LEDOK' width={46} height={46} />
-            </div>
-        </Midias>
+      <Img src={Logo} alt="Logo LEDOK - Comunição em Leds" />
+      <Number>
+        <p>ledokadm@gmail.com</p>
+        <p>(51)9 96301314</p>
+      </Number>
+      <Midias>
+        <p>Nos acompanhe :</p>
+        <div>
+          <LinkMidia
+            href="https://www.facebook.com/profile.php?id=100091456367460"
+            target="_blank"
+          >
+            <Image src={Facebook} alt="Facebook LEDOK" width={34} height={34} />
+          </LinkMidia>
+          <LinkMidia
+            href="https://www.instagram.com/ledokleds/"
+            target="_blank"
+          >
+            <Image
+              src={Instagram}
+              alt="Instagram LEDOK"
+              width={46}
+              height={46}
+            />
+          </LinkMidia>
+        </div>
+      </Midias>
     </Container>
-  )
+  );
 }
 
 const Container = styled.footer`
-    background-color: #221F20;
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    padding: 30px;
+  background-color: #221f20;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  padding: 30px;
 
-    @media(max-width: 740px){
-        padding: 50px;
-    }
-`
+  @media (max-width: 740px) {
+    padding: 50px;
+  }
+`;
 
 const Img = styled(Image)`
-    width: 200px;
-    height: 51.41px;
-`
+  width: 200px;
+  height: 51.41px;
+`;
 
 const Number = styled.div`
-    p {
-        color: #fff9f9;
-        font-weight: 300;
-    }
-    p:last-of-type{
-        font-size: 14pt;
-        padding-top: 3px;
-    }
-    @media(max-width: 740px){
-        text-align: center;
-        padding-top: 20px;
-        padding-bottom: 20px;
-    }
-`
+  p {
+    color: #fff9f9;
+    font-weight: 300;
+  }
+  p:last-of-type {
+    font-size: 14pt;
+    padding-top: 3px;
+  }
+  @media (max-width: 740px) {
+    text-align: center;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+`;
 
 const Midias = styled.div`
-     p {
-        color: #fff9f9;
-        font-weight: 300;
+  p {
+    color: #fff9f9;
+    font-weight: 300;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    @media (max-width: 740px) {
+      justify-content: center;
     }
-    div {
-        display: flex;
-        align-items: center;
-        @media(max-width: 740px){
-            justify-content: center;
-        }
-    }
-`
+  }
+`;
+
+const LinkMidia = styled.a`
+  cursor: pointer;
+`;
--- app/components/headline/Slide.tsx
+++ app/components/headline/Slide.tsx
@@ -1,55 +1,68 @@
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import Plate1 from '../../../public/placa1.png'
-import Plate2 from '../../../public/placa2.png'
-import Plate3 from '../../../public/placa3.png'
-import Plate4 from '../../../public/placa4.png'
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Plate1 from "../../../public/placa1.png";
+import Plate2 from "../../../public/placa2.png";
+import Plate3 from "../../../public/placa33.png";
+import Plate4 from "../../../public/placa4.png";
 
 export default function Slide() {
-    const [ counter, setCounter ] = useState(1)
+  const [counter, setCounter] = useState(1);
 
-    const increment = () => {
-        setCounter(counter < 4 ? counter + 1 : 1 )
-    }
-    
-        useEffect(() => {
-            const interval = setInterval(increment, 4000);
-            return () => clearInterval(interval);
-          }, [ counter ]);
-        
+  const increment = () => {
+    setCounter(counter < 4 ? counter + 1 : 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(increment, 4000);
+    return () => clearInterval(interval);
+  }, [counter]);
 
   return (
     <Container>
-        <Img src={Plate4} alt='Placa em LED - LEDOK' className={`img ${ counter === 1 && 'show' }`} />
-        <Img src={Plate2} alt='Placa em LED - LEDOK' className={`img ${ counter === 2 && 'show' }`} />
-        <Img src={Plate3} alt='Placa em LED - LEDOK' className={`img ${ counter === 3 && 'show' }`} />
-        <Img src={Plate1} alt='Placa em LED - LEDOK' className={`img ${ counter === 4 && 'show' }`} />
+      <Img
+        src={Plate4}
+        alt="Placa em LED - LEDOK"
+        className={`img ${counter === 1 && "show"}`}
+      />
+      <Img
+        src={Plate2}
+        alt="Placa em LED - LEDOK"
+        className={`img ${counter === 2 && "show"}`}
+      />
+      <Img
+        src={Plate3}
+        alt="Placa em LED - LEDOK"
+        className={`img ${counter === 3 && "show"}`}
+      />
+      <Img
+        src={Plate1}
+        alt="Placa em LED - LEDOK"
+        className={`img ${counter === 4 && "show"}`}
+      />
     </Container>
-  )
+  );
 }
 
 const Container = styled("div")`
-     .img {
-        opacity: 0;
-        color: #fff;
-        transition: 0.8s;
-        position: absolute;
-        top: 64px;
-        left: 0;
-        width: 874.84px;
-        height: 400px;
-        @media(max-width: 900px){
-            width: 100%;
-            height: auto;
-        }
+  .img {
+    opacity: 0;
+    color: #fff;
+    transition: 0.8s;
+    position: absolute;
+    top: 64px;
+    left: 0;
+    width: 874.84px;
+    height: 400px;
+    @media (max-width: 900px) {
+      width: 100%;
+      height: auto;
     }
+  }
 
-    .show {
-        opacity: 1;
-    }
-`
+  .show {
+    opacity: 1;
+  }
+`;
 
-const Img = styled(Image)`
-    
-`
+const Img = styled(Image)``;
